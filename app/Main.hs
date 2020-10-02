edd514a9b8d3049cfdea582ded0c99f9f19dbff7
mitsuji <tkms@mitsuji.org>
2020-10-02 14:27:54 +0900

    fix: invalid frameCount in macOS

--- app/Main.hs
+++ app/Main.hs
@@ -12,21 +12,20 @@ main = runProc $ def { procSetup      = setup
                      , procKeyPressed = keyPressed
                      }
 
-type State = (Std15, IORef Bool, Int)
+type State = (Std15, Word, IORef Bool, Int)
 
 setup :: Pio State
 setup = do
   size (512, 384)
   std15 <- IJ.newStd15 512 384 32 24
   running <- liftIO $ newIORef True
-  return (std15,running,15)
+  return (std15,0,running,15)
 
 update :: Update State
-update state@(std15,running,x) = do
+update (std15,frame,running,x) = do
   isRunning <- liftIO $ readIORef running
   when isRunning $ do
-    fc <- frameCount
-    when (fc `mod` 5 == 0) $ do
+    when (frame `mod` 2 == 0) $ do
       IJ.locate std15 x 5
       IJ.putc std15 '0'
       random 32 >>= \rnd -> IJ.locate std15 (floor rnd) 23
@@ -36,19 +35,19 @@ update state@(std15,running,x) = do
       when (sc /= '\0') $ do
         IJ.locate std15 0 23
         IJ.putstr std15 "Game Over..."
-        IJ.putnum std15 fc
+        IJ.putnum std15 $ fromIntegral frame
         liftIO $ writeIORef running False
-  return state
+  return (std15,frame+1,running,x)
 
 draw :: State -> Draw
-draw (std15,_,_) = IJ.drawScreen std15
+draw (std15,_,_,_) = IJ.drawScreen std15
 
 keyPressed :: Update State
-keyPressed (std15,running,x) = do
+keyPressed (std15,frame,running,x) = do
   k <- key
   let x' = case k of
         SpecialKey KeyLeft  -> x-1
         SpecialKey KeyRight -> x+1
         _ -> x
-  return (std15,running,x')
+  return (std15,frame,running,x')
   
